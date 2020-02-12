0e6ffe6ce085689bafb4f32a1eaea54fd179134d
Synpheros <Synpheros@hotmail.com>
2020-02-12 20:20:18 +0100

    Fixed when returning single activity result

--- src/lib/activities/LimeSurveyActivity.js
+++ src/lib/activities/LimeSurveyActivity.js
@@ -351,10 +351,10 @@ class LimeSurveyActivity extends Activity {
 			}
 		}
 
-		console.log(results);
-
 		for (let i = 0; i < participants.length; i++) {
-			results[participants[i]] = (results[participants[i]] !== null && results[participants[i]].submitdate !== null);
+
+			results[participants[i]] = (results[participants[i]] !== null
+				&& (results[participants[i]].submitdate !== null && results[participants[i]].submitdate !== undefined ));
 		}
 		console.log(results);
 
