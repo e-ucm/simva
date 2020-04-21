565ae9aa34b1901cef458e95ad6e60dc2498ab76
Synpheros <Synpheros@hotmail.com>
2020-04-21 09:14:31 +0200

    Created the KeycloakKeyManager to manage keycloak public keys

--- src/lib/userscontroller.js
+++ src/lib/userscontroller.js
@@ -1,19 +1,14 @@
 const ServerError = require('./error');
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
-const KeyCloakCerts = require('get-keycloak-public-key');
+const KeycloakKeyManager = require('./utils/keycloakkeymanager');
 const jwt = require('jsonwebtoken');
  
 var config = require('./config');
 
-const keyCloakCerts = new KeyCloakCerts(config.sso.url, config.sso.realm);
-
-console.log(config.sso.url, config.sso.realm, keyCloakCerts);
-
 var UsersController = {};
 
 var secretKey = 'th1s_15_a_tmporall7_k3y';
-
 
 UsersController.getUser = async (id) => {
 	var res = await mongoose.model('user').find({_id: id});
@@ -46,6 +41,16 @@ UsersController.addUser = async (params) => {
 	await user.save();
 
 	return user;
+}
+
+UsersController.updateUser = async (id, params) => {
+	let result =  await mongoose.model('user').updateOne({ _id: id }, params);
+
+	if(result.ok !== result.n){
+		throw {message: 'There was an error in the study.'};
+	}
+
+	return params;
 }
 
 UsersController.authUser = async (username, plainPass) => {
@@ -127,58 +132,46 @@ UsersController.validateJWT = async (token) => {
 		if(decoded && decoded.header && decoded.payload && decoded.payload.iss){
 			switch(decoded.payload.iss){
 				case config.sso.realmUrl:
-					keyCloakCerts.fetch(decoded.header.kid)
-						.then((publicKey) => {
-							jwt.verify(token, 'publicKey', function(error, decoded) {
-								if(error){
-									console.log(error);
-									reject(error);
-								}else{
-									UsersController.getUsers({ email: decoded.email })
-										.then((users) => {
-											if(users.length !== 0){
-												resolve({ data: users[0] });
-											}else{
-												let user = {
-													username: decoded.preferred_username,
-													password: Math.random().toString(36).slice(-8),
-													email: decoded.email,
-													role: 'student'
-												};
-
-												for (var i = decoded.realm_access.roles.length - 1; i >= 0; i--) {
-													if(decoded.realm_access.roles[i] === 'teacher' || decoded.realm_access.roles[i] === 'researcher'){
-														user.role = 'teacher';
-														break;
-													};
-												}
-
-												UsersController.addUser(user)
-													.then((result) => {
-														resolve({ data: {
-															_id: result._id,
-															username: result.username,
-															email: result.email,
-															role: result.role
-														}});
-													})
-													.catch((error) => {
-														console.log(error);
-														reject(error);
-													});
-											}
-										})
-										.catch((error) => {
-											console.log(error);
-											reject(error);
-										});
-								}
+					let ValidateToken = function(){
+						KeycloakKeyManager.getKey(decoded.header.kid)
+							.then((publicKey) => {
+								jwt.verify(token, publicKey, function(error, decoded) {
+									if(error){
+										console.log(error);
+										reject(error);
+									}else{
+										UsersController.CreateOrUpdateKeycloakUser(decoded)
+											.then((result) => {
+												resolve(result);
+											})
+											.catch((error) => {
+												reject(error);
+											});
+									}
+								});
+							})
+							.catch((error) => {
+								console.log(error);
+								reject(error);
 							});
+					}
+
+					KeycloakKeyManager.checkKey(decoded.header.kid, token)
+						.then(() => {
+							ValidateToken();
 						})
 						.catch((error) => {
-							console.log(error);
-							reject(error);
-						});
+							console.log('FAILED VALIDATION');
+							KeycloakKeyManager.reloadKey(decoded.header.kid)
+								.then((publicKey) => {
+									ValidateToken();
+								})
+								.catch((error) => {
+									console.log(error);
+									reject(error);
+								})
+						})
+					
 
 					break;
 				case 'simva':
@@ -197,6 +190,76 @@ UsersController.validateJWT = async (token) => {
 		}
 	});
 	
+}
+
+UsersController.CreateOrUpdateKeycloakUser = async function (decoded){
+	return new Promise((resolve, reject) => {
+		UsersController.getUsers({ email: decoded.email })
+			.then((users) => {
+				if(users.length !== 0){
+					if(users[0].role !== UsersController.getRoleFromJWT(decoded)){
+						UsersController.updateUser()
+							.then((result) => {
+								resolve(UsersController.simplifyUser(result));
+							})
+							.catch((error) => {
+								console.log(error);
+								reject(error);
+							})
+					}else{
+						resolve(UsersController.simplifyUser(users[0]))
+					}
+				}else{
+					createUserFromJWT(decoded)
+						.then((result) => {
+							resolve(UsersController.simplifyUser(result));
+						})
+						.catch((error) => {
+							console.log(error);
+							reject(error);
+						})
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+				reject(error);
+			});
+	});
+}
+
+UsersController.simplifyUser = function(user){
+	return { data: {
+		_id: user._id,
+		username: user.username,
+		email: user.email,
+		role: user.role
+	}}
+}
+
+UsersController.createUserFromJWT = async function(decoded){
+	let user = {
+		username: decoded.preferred_username,
+		password: Math.random().toString(36).slice(-8),
+		email: decoded.email,
+		role: 'student'
+	};
+
+	user.role = UsersController.getRoleFromJWT(decoded);
+
+	return await UsersController.addUser(user);
+}
+
+UsersController.getRoleFromJWT = function(decoded){
+	let role = 'student';
+
+	for (var i = decoded.realm_access.roles.length - 1; i >= 0; i--) {
+		if(decoded.realm_access.roles[i] === 'teacher' || decoded.realm_access.roles[i] === 'researcher'){
+			role = 'teacher';
+			break;
+		};
+	}
+
+	return role;
 }
 
 
---
+++ src/lib/utils/keycloakkeymanager.js
@@ -0,0 +1,65 @@
+const KeyCloakCerts = require('get-keycloak-public-key');
+const jwt = require('jsonwebtoken');
+var config = require('../config');
+
+
+var KeycloakKeyManager = {};
+
+const keyCloakCerts = new KeyCloakCerts(config.sso.url, config.sso.realm);
+
+let LoadedKeys = {};
+
+KeycloakKeyManager.getKey = async function(kid){
+	try{
+		if(!LoadedKeys.hasOwnProperty(kid)){
+			LoadedKeys[kid] = await KeycloakKeyManager.reloadKey(kid);
+		}
+	}catch(e){
+		console.log(e)
+	}
+
+	return LoadedKeys[kid];
+}
+
+KeycloakKeyManager.checkKey = async function(kid, token){
+	return new Promise((resolve, reject) => {
+		try{
+			KeycloakKeyManager.getKey(kid)
+				.then((privateKey) => {
+					jwt.verify(token, privateKey, function(error, decoded) {
+						if(error && error.message === 'invalid algorithm'){
+							reject('Not valid signature');
+						}else{
+							resolve();
+						}
+					});
+				})
+				.catch((error) => {
+					reject(error);
+				})
+		}catch(e){
+			console.log(e)
+			reject(e);
+		}
+	});
+}
+
+KeycloakKeyManager.reloadKey = async function(kid){
+	return new Promise((resolve, reject) => {
+		console.log('######### RELOADING KEYCLOAK KEY ' + kid + ' #########');
+		try{
+			keyCloakCerts.fetch(kid)
+				.then((publicKey) => {
+					resolve(publicKey);
+				})
+				.catch((error) => {
+					reject(error);
+				})
+		}catch(e){
+			console.log(e)
+			reject(e);
+		}
+	})
+}
+
+module.exports = KeycloakKeyManager;
