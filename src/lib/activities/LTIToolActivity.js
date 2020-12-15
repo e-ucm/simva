3b2477b05c58df9950ededd6bbf400fcbd868c0d
Synpheros <Synpheros@hotmail.com>
2020-12-15 06:50:08 +0100

    Added LTI tool activity

--- src/lib/activities/LTIToolActivity.js
+++ src/lib/activities/LTIToolActivity.js
@@ -5,6 +5,7 @@ let mongoose = require('mongoose');
 let fs = require('fs');
 let ObjectId = mongoose.Types.ObjectId;
 let app = require('../utils/appmanager').getApp();
+let LtiController = require('../lticontroller');
 
 const validator = require('../utils/validator');
 
@@ -64,13 +65,10 @@ class LTIToolActivity extends Activity {
 	// ##########################################
 
 	constructor(params){
-		this.extra_data = {};
-		this.owners = [];
+		super(params);
 
-		if(ObjectId.isValid(params)){
-			this._id = params;
-		}else if(typeof params == 'object'){
-			this.params = params;
+		if(params.tool){
+			this.extra_data.tool = params.tool;
 		}
 	}
 
@@ -104,74 +102,22 @@ class LTIToolActivity extends Activity {
 		return {};
 	}
 
-	set params(params){
-		for(var p in activityschema.properties){
-			if(params[p]){
-				this[p] = params[p]
-			}
-		}
-
-		if(params['_id']){
-			this._id = params['_id'];
-		}
-	}
-
-
-	set id(id){
-		if(!ObjectId.isValid(id)){
-			throw {message: 'not a valid objectId'};
-		}else{
-			this._id = id;
-		}
-	}
-
-	get id(){
-		return this._id;
-	}
-
 	async load(){
-		var res = await mongoose.model('activity').find({_id: this.id});
-
-		if(res.length > 0) {
-			for(var k in res[0]){
-				this[k] = res[0][k];
-			}
-			return true;
-		}
-
-		return false;
+		return super.load();
 	}
 
 	async save(){
-		var params = {};
+		let tool = await LtiController.getLtiTool(this.extra_data.tool);
 
-		for(var p in activityschema.properties){
-			params[p] = this[p];
+		if(!tool){
+			throw { message: 'Lti tool not found!' };
 		}
 
-		if(this.id){
-			params['_id'] = this.id;
-		}
-
-		if(params._id){
-			var result = await mongoose.model('activity').updateOne({ _id: this.id }, params);
-			if(result.ok !== result.n){
-				throw { message: 'Error saving the activity' };
-			}
-		}else{
-			var activity = new mongoose.model('activity')(params);
-			await activity.save();
-			this.id = activity._id;
-		}
-		
-
-		return true;
+		return await super.save();
 	}
 
 	async remove(){
-		var res = await mongoose.model('activity').deleteOne({_id: this.id});
-
-		return true;
+		return super.remove();
 	}
 
 	// ##########################################
@@ -183,35 +129,51 @@ class LTIToolActivity extends Activity {
 	}
 
 	async addParticipants(participants){
-		return null;
+		return await super.addParticipants(participants);
 	}
 
 	async removeParticipants(participants){
-		return null;
+		return await super.removeParticipants(participants);
 	}
 
 	async setResult(participant, result){
-		return null;
+		return await super.setResult(participant, result);
 	}
 
 	async getResults(participants){
-		return null;
+		return await super.getResults(participants);
 	}
 
 	async setCompletion(participant, status){
-		return null;
+		return await super.setCompletion(participant, status);
 	}
 
 	async getCompletion(participants){
-		return null;
+		return await super.getCompletion(participants);
 	}
 
 	async target(participant){
-		return false;
-	}
+		let tool = await LtiController.getLtiTool(this.extra_data.tool);
 
-	open(res, participant){
-		return false;
+		let login_hint = await LtiController.generateJWT({
+			participant: participant
+		});
+
+		let lti_message_hint = await LtiController.generateJWT({
+			activity: this.id
+		});
+
+		let deployment = 'D001';
+
+		let url = tool.login_url
+			+ '?login_hint=' + login_hint
+			+ '&lti_message_hint=' + lti_message_hint
+			+ '&client_id=' + tool.client_id
+			+ '&lti_deployment_id=' + deployment
+			+ '&iss=' + config.sso.realmUrl
+			+ '&target_link_uri=' + tool.url;
+
+		return url;
 	}
 };
 
