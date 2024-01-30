1cc019c7a5a54e20a7056392d0a72821277badd9
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-01-30 13:11:27 +0100

    relative path in node-module folder

--- docker-startup.sh
+++ docker-startup.sh
@@ -2,17 +2,17 @@
 set -euo pipefail
 [[ "${DEBUG:-false}" == "true" ]] && set -x
 
-if [[ ! -d "node_modules" ]]; then
+if [[ ! -d "./node_modules" ]]; then
   npm install
-  cksum "./package.json" | cut -d' ' -f1 >> "node_modules/.checksum"
+  cksum "./package.json" | cut -d' ' -f1 >> "./node_modules/.checksum"
 else 
-  OldSum=$(cat "node_modules/.checksum")
+  OldSum=$(cat "./node_modules/.checksum")
   NewSum=$(cksum "./package.json" | cut -d' ' -f1)
   if [[ $OldSum != $NewSum ]]; then
     # Checksum mismatch, do something useful here
-    rm -rf "node_modules"
+    rm -rf "./node_modules"
     npm install
-    echo $NewSum >> "node_modules/.checksum"
+    echo $NewSum >> "./node_modules/.checksum"
   fi
 fi
 
