355391baef0f771b986dafe578307e040c2f09ff
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-03-11 08:50:59 +0100

    fix pdf generation

--- docker-startup.sh
+++ docker-startup.sh
@@ -6,6 +6,11 @@ if [[ ! -d "./node_modules" ]]; then
   npm install
 fi
 
+apt update -y
+apt --fix-broken install -y
+apt install -y ca-certificates fonts-liberation libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2  libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc-s1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
+dpkg --configure -a
+
 if [[ "${NODE_ENV:-production}" == "development" ]]; then
   npm install -g nodemon
   npm install -g bunyan
