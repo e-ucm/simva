5a2244e9c655b4542434eef2bd92d84e3f2f592d
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2024-10-24 09:44:16 +0200

    functions in tokens hmacKey

--- src/api/routes/studies.js
+++ src/api/routes/studies.js
@@ -10,7 +10,8 @@ validator.addValidations('/studies', router);
 const Authenticator = require('../../lib/utils/authenticator');
 const logger = require('../../lib/logger');
 const config = require('../../lib/config');
-const { verifyMessage, signMessage, createHMACKey } = require("../../lib/utils/hMacKey/crypto.js");
+const { createHMACKey } = require("../../lib/utils/hMacKey/crypto.js");
+const { validateUrl, createUrl } = require("../../lib/utils/hMacKey/tokens.js");
 
 initHmacKey();
 
@@ -255,52 +256,6 @@ router.get('/:studyid/events/getPresignedUrl', Authenticator.auth, async (req, r
       next(err);
   }
 });
-
-async function validateUrl(url, query, hmacKey) {
-  const signature = query.signature;
-  console.log(signature);
-  var toSign=Object.entries(query)
-          .filter(([key, value])=> key !== "signature")
-          .sort(([keyA], [keyB]) => keyA.localeCompare(keyB)) // Sort by keys
-          .map(([key, value]) => `${key}=${value}`)
-          .join('\n');
-  toSign= url + '\n' + toSign;
-  console.log(toSign);
-  if(verifyMessage(toSign, signature, hmacKey)) {
-    console.log("Valid signature !");
-    return true;
-  } else {
-    console.log("Invalid signature !");
-    return false;
-  }
-};
-
-
-async function createUrl(url, mapParameters, hmacKey) {
-  const buffer = new Uint8Array(8);
-  crypto.getRandomValues(buffer);
-
-  mapParameters.ts = (new Date()).toISOString();
-  var toSign=Object.entries(mapParameters)
-          .sort(([keyA], [keyB]) => keyA.localeCompare(keyB)) // Sort by keys
-          .map(([key, value]) => `${key}=${value}`)
-          .join('\n');
-  toSign=url + '\n' + toSign;
-  var signature;
-  try {
-     signature = await signMessage(toSign, hmacKey);
-  } catch(e) {
-      console.log(e);
-      signature = "TODO";
-  }
-  const queryString = Object.entries(mapParameters)
-      .sort(([keyA], [keyB]) => keyA.localeCompare(keyB)) // Sort by keys
-      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-      .join('&');
-      url = url + `?${queryString}&signature=${signature}`;
-
-  return { data : {url : url} };
-}
 
 
 /**
