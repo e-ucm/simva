a5d57215cb9fcb1d8dced80efb1809ddf3670e29
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-03-27 10:10:23 +0100

    remove profiling

--- src/lib/profiling.js
+++ src/lib/profiling.js
@@ -5,14 +5,4 @@ const ms = require('ms');
 
 if(process.env.NODE_ENV == "development" && config.api.profiling) {
   logger.info("Profiling in progress...");
-  const profilingFolder = process.env.PROFILING_FOLDER || path.join(__dirname, '../../../profiling');
-  // Schedule a task to run every x
-  function profiling() {
-    logger.info(`schedule task for profiling running...`);
-    //let filename=`${profilingFolder}/Heap.${now().toISOString()}.heapsnapshot`;
-    let filename=`${profilingFolder}/${require('v8').writeHeapSnapshot()}`;
-    logger.info(`Saved heapdump into ${require('v8').writeHeapSnapshot(filename)}`);
-    setTimeout(profiling, ms("30min"));
-  }
-  profiling();
 }
